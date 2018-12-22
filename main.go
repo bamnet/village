897311f239b0b62f2f14569e97e4db0b4ec03aad
Brian Michalski <bmichalski@gmail.com>
2018-12-22 05:39:13 +0000

    Proof of concept scheduler.

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"math/rand"
 	"net/http"
+	"sync"
 	"time"
 )
 
@@ -17,6 +18,13 @@ var (
 	host = flag.String("host", "", "IP / hostname of the village controller")
 )
 
+// Event represents a scheduled state change for a light.
+type Event struct {
+	ID         int
+	Brightness int
+	Time       time.Time
+}
+
 func init() {
 	rand.Seed(time.Now().UTC().UnixNano())
 }
@@ -24,19 +32,29 @@ func init() {
 func main() {
 	flag.Parse()
 
-	for {
-		id := rand.Intn(9) + 1
-		b := rand.Intn(100)
-		if err := setLight(id, b); err != nil {
-			log.Fatalf("HTTP error: %v", err)
-		}
-		time.Sleep(time.Duration(rand.Int63n(150)) * time.Millisecond)
+	schedule := []Event{
+		{1, 100, time.Now().Add(1 * time.Second)},
+		{1, 25, time.Now().Add(2 * time.Second)},
 	}
+
+	wg := sync.WaitGroup{}
+	wg.Add(len(schedule))
+	for _, e := range schedule {
+		wait := e.Time.Sub(time.Now())
+		t := time.NewTimer(wait)
+		go func(id, b int) {
+			defer wg.Done()
+			<-t.C
+			setLight(id, b)
+		}(e.ID, e.Brightness)
+	}
+	wg.Wait()
 }
 
 // setLight immediately changes the brightness of a light.
 // brightness should be [0, 100].
 func setLight(id, brightness int) error {
+	log.Printf("Setting %d to %d", id, brightness)
 	b := MaxBrightness * brightness / 100.0
 	url := fmt.Sprintf("http://%s/console/send?text=%d%%2C%d%%0A", *host, id, b)
 	_, err := http.Get(url)
